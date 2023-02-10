bbba1dd90b986c36bb4ca3a2b724c656f64f902b
Aaron <aaronlee@5051.com>
2023-02-10 17:06:22 +0800

    Update 4
    Co-authored-by: Darance Ng Zhuo Rui /IT <s10243476@connect.np.edu.sg>
    Co-authored-by: DaranceNg <DaranceNg@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 const wrapper = document.querySelector(".sliderWrapper")
 const menuItems = document.querySelectorAll(".menuItem")
 
+
+const products = [{id: 1,title:"VARMBLIXT",price:89.90,Quantities:{img:"Images/Photo2.png"}},
+{id: 2,title:"BLAVINGAD",price:69.90,Quantities:{img:"Images/BLAVINGAD.png"}},
+{id: 3,title:"OBEGRANSAD",price:129.90,Quantities:{img:"Images/OBEGRANSAD.png"}},
+{id: 4,title:"BYGGLEK",price:89.90,Quantities:{img:"Images/BYGGLEK.png"}}
+]
+
+let chosenProduct = products[0]
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductQuantity = document.querySelector(".Quantities");
+
+
+
+
 menuItems.forEach((item,index)=>{
   item.addEventListener("click", () =>{
     console.log("you clicked!" + index);
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change chosen product
+    chosenProduct= products[index]
+
+    //change text of currentProduct
+    currentProductTitle.textContent = chosenProduct.title
+    currentProductPrice.textContent = "$" + chosenProduct.price +"0"
+    currentProductImg.src = chosenProduct.Quantities.img
+    
+
   })
+})
+
+
+const productButton = document.querySelector(".ProductButton");
+const payment = document.querySelector(".payment");
+const Close = document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+  payment.style.display="flex"
+})
+
+Close.addEventListener("click",()=>{
+  payment.style.display="none"
 })
--- testgame.js
+++ testgame.js
@@ -1,14 +1,81 @@
-const spinButton = document.querySelector("#spin-button");
-const result = document.querySelector("#result");
-const wheel = document.querySelector(".wheel");
+let flippedCards = [];
+let lockBoard = false;
+let pairsLeft = 6;
 
-spinButton.addEventListener("click", function() {
-  let randomDegree = Math.floor(Math.random() * 360);
-  wheel.style.transform = `rotate(${randomDegree}deg)`;
+let cards = document.querySelectorAll(".card");
 
-  let rewards = [1, 2, 3, 4, 5, 6];
-  let sliceDegree = 360 / rewards.length;
+cards = Array.from(cards);
 
-  let resultIndex = Math.floor(randomDegree / sliceDegree);
-  result.textContent = `You won: ${rewards[resultIndex]}`;
-});
+cards = shuffle(cards);
+
+let board = document.querySelector(".board");
+board.innerHTML = "";
+for (let card of cards) {
+  board.appendChild(card);
+  card.addEventListener("click", flipCard);
+}
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === flippedCards[0]) return;
+  
+  this.classList.add("flipped");
+
+  if (flippedCards.length === 0) {
+    flippedCards[0] = this;
+    return;
+  } else {
+    lockBoard = true;
+    flippedCards[1] = this;
+    checkForMatch();
+  }
+}
+
+function checkForMatch() {
+  let isMatch = flippedCards[0].dataset.card === flippedCards[1].dataset.card;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  flippedCards[0].removeEventListener("click", flipCard);
+  flippedCards[1].removeEventListener("click", flipCard);
+  pairsLeft--;
+  if (pairsLeft === 0) {
+    showGameOver();
+  }
+  resetBoard();
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    flippedCards[0].classList.remove("flipped");
+    flippedCards[1].classList.remove("flipped");
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  flippedCards = [];
+  lockBoard = false;
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function showGameOver() {
+  let gameOver = confirm("Congratulations, you won! Play again?");
+  if (gameOver) {
+    window.location.reload();
+  } else {
+    window.close();
+  }
+}
