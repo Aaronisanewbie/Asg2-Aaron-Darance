ff6c997d3a2458ae9986c4377bdd2c2a6f12119c
Aaron <aaronlee@5051.com>
2023-02-10 21:40:52 +0800

    tidying up code

--- script.js
+++ script.js
@@ -66,3 +66,21 @@ fButton.addEventListener("click",()=>{
   
 })
 
+var jsondata = {"field1": "xyz","field2": "abc"};
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://aarondaranceid-2daa.restdb.io/rest/contacts",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "63e64767478852088da68029",
+    "cache-control": "no-cache"
+  },
+  "processData": false,
+  "data": JSON.stringify(jsondata)
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
